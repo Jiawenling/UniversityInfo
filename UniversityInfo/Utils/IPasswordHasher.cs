dafd375cc57f34e114942b73dc6e43dc89ba28cb
jiawen.ling <jling002@e.ntu.edu.sg>
2023-07-07 10:13:06 +0800

    removed utils

--- UniversityInfo/Utils/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace UniversityInfo.Utils
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool VerifyPassword(string password, string passwordHash);
-    }
-}
